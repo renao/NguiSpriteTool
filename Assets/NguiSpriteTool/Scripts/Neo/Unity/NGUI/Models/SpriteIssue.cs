2653ecfa9a449c57db1b88c6d8b770d402d1e326
René Wernicke <rw@neopoly.de>
2015-10-12 16:41:03 +0200

    Add SpriteIssue model

---
+++ Assets/NguiSpriteTool/Scripts/Neo/Unity/NGUI/Models/SpriteIssue.cs
@@ -0,0 +1,21 @@
+﻿namespace Neo.Unity.NGUI.Models {
+  public class SpriteIssue {
+
+    public UISprite Sprite;
+    public UISpriteInconsistence Inconsistence;
+    public string PrefabLocation;
+
+
+    public SpriteIssue(UISprite sprite, UISpriteInconsistence inconsistence, string location) {
+      Sprite = sprite;
+      Inconsistence = inconsistence;
+      PrefabLocation = location;
+    }
+  }
+
+
+  public enum UISpriteInconsistence {
+    AtlasNotFound,
+    NoAtlasMember
+  }
+}
