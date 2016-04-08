a7b71bfe9e3653eaffd4616a449a48bbb0abb0cc
René Wernicke <rw@neopoly.de>
2016-04-08 11:54:07 +0200

    Add PathCombiner to toolset

---
+++ Assets/TinyTools/PathCombiner/Scripts/Neo/IO/PathCombiner.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace Neo.IO {
+  public class PathCombiner {
+
+    public static string Combine(params string[] parts) {
+      string path = string.Empty;
+      for (int i = 0, imax = parts.Length; i < imax; i++) {
+        path = Path.Combine(path, parts[i]);
+      }
+      return path;
+    }
+  }
+}
---
+++ Assets/TinyTools/PathCombiner/Tests/Editor/Neo/IO/TestPathCombiner.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Neo.IO;
+using System.IO;
+
+namespace Tests.Neo.IO {
+
+  [TestFixture]
+  public class TestsPathCombiner {
+
+    [Test]
+    public void CombinesPartsToQualifiedPath() {
+      string part1 = "first";
+      string part2 = "second";
+      string part3 = "last.cs";
+
+      string expected = Path.Combine(Path.Combine(part1, part2), part3);
+      Assert.AreEqual(expected, PathCombiner.Combine(part1, part2, part3));
+    }
+  }
+}
