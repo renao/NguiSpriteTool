8aeff4b8cb42d7f9135433403056a425311abc9a
René Wernicke <rw@neopoly.de>
2016-04-21 15:25:52 +0200

    Add simple scene support in SpriteTool

--- Assets/NguiSpriteTool/Scripts/Neo/Unity/Editor/NGUI/SpriteTool.cs
+++ Assets/NguiSpriteTool/Scripts/Neo/Unity/Editor/NGUI/SpriteTool.cs
@@ -3,7 +3,8 @@ using System;
 using System.Collections.Generic;
 using Neo.IO;
 using Neo.Unity.NGUI.Models;
-
+using SceneManager = UnityEditor.SceneManagement.EditorSceneManager;
+using UnityEngine.SceneManagement;
 
 namespace Neo.Unity.NGUI {
   public class SpriteTool {
@@ -22,20 +23,48 @@ namespace Neo.Unity.NGUI {
     }
 
     private void fetchUsedSprites() {
+      fetchSpritesFromPrefabs();
+      fetchSpritesFromScenes();
+    }
+
+    private void fetchSpritesFromPrefabs() {
       List<string> prefabLocations = FileCrawler.FetchFilesRecursively(dataPath, "prefab");
 
       foreach(string location in prefabLocations) {
         string loc = location.Replace(@"\", "/").Replace(Application.dataPath, "Assets");
         GameObject go = UnityEditor.AssetDatabase.LoadAssetAtPath<GameObject>(loc);
-        if (go != null) fetchSpritesFrom(go, loc);
+        if(go != null) {
+          fetchSpritesFromGameObject(go, loc);
+        }
       }
     }
 
-    private void fetchSpritesFrom(GameObject go, string location) {
+    private void fetchSpritesFromScenes() {
+      List<string> sceneLocations = FileCrawler.FetchFilesRecursively(dataPath, "unity");
+
+      foreach(string location in sceneLocations) {
+        string loc = location.Replace(@"\", "/").Replace(Application.dataPath, "Assets");
+        Scene currentScene = SceneManager.OpenScene(loc, UnityEditor.SceneManagement.OpenSceneMode.Additive);
+        fetchSpritesFromScene(currentScene);
+        SceneManager.CloseScene(currentScene, true);
+      }
+    }
+
+    private void fetchSpritesFromGameObject(GameObject go, string location) {
       UISprite[] sprites = go.GetComponentsInChildren<UISprite>(true);
       if(sprites != null) {
         foreach(UISprite sprite in sprites) Info.AddSprite(sprite, location);
       }
+    }
+
+    private void fetchSpritesFromScene(Scene scene) {
+      scene.GetRootGameObjects().ForEach((parent) => {
+        fetchSpritesFromGameObject(parent, sceneGameObjectPath(scene, parent));
+      });
+    }
+
+    private string sceneGameObjectPath(Scene scene, GameObject go) {
+      return string.Format("{0} => {1}", scene.name, go.name);
     }
   }
 }
