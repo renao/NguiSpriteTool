dc90653502122915b1d75774685a8d1535ad31bf
René Wernicke <rw@neopoly.de>
2016-04-25 16:51:54 +0200

    Add Colorizer to Neo.UI

---
+++ Assets/TinyTools/Colorizer/Scripts/Neo/UI/Colorizer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Neo.UI {
+  public class Colorizer {
+
+    public static Color Invert(Color color) {
+      return new Color(
+          r: invertFloatColorComponent(color.r),
+          g: invertFloatColorComponent(color.g),
+          b: invertFloatColorComponent(color.b),
+          a: color.a
+        );
+    }
+
+    public static Color32 Invert(Color32 color) {
+      return new Color32(
+        r: invertByteColorComponent(color.r),
+        g: invertByteColorComponent(color.g),
+        b: invertByteColorComponent(color.b),
+        a: color.a
+      );
+    }
+
+    private static byte invertByteColorComponent(byte componentValue, byte componentMax=0xFF) {
+      return (byte) (componentMax - componentValue);
+    }
+
+    private static float invertFloatColorComponent(float componentValue, float componentMax=1f) {
+      return componentMax - componentValue;
+    }
+  }
+}
---
+++ Assets/TinyTools/Colorizer/Tests/Editor/Neo/UI/TestColorizer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Neo.UI;
+using NUnit.Framework;
+
+namespace Tests.Neo.UI {
+
+  [TestFixture]
+  public class TestColorizer {
+
+    private Color black, gray, white;
+    private Color32 byteBlack, byteGray, byteWhite;
+
+
+    [SetUp]
+    public void SetUp() {
+      black = Color.black;
+      gray = Color.gray;
+      white = Color.white;
+
+      byteBlack = new Color32(0, 0, 0, 255);
+      byteGray = new Color32(128, 128, 128, 255);
+      byteWhite = new Color32(255, 255, 255, 255);
+    }
+
+    [Test]
+    public void InvertsColors() {
+      Assert.AreEqual(Color.white, Colorizer.Invert(black));
+      Assert.AreEqual(Color.gray,  Colorizer.Invert(gray));
+      Assert.AreEqual(Color.black, Colorizer.Invert(white));
+    }
+
+    [Test]
+    public void InvertsByteColors() {
+      Assert.AreEqual(byteWhite, Colorizer.Invert(byteBlack));
+      Assert.AreEqual(new Color32(127, 127, 127, 255), Colorizer.Invert(byteGray));
+      Assert.AreEqual(byteBlack, Colorizer.Invert(byteWhite));
+    }
+  }
+}
