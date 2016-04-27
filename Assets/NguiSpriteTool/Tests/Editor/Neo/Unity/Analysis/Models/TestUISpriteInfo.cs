88c1cd8122fa03d16815c532c51ae871271f026a
René Wernicke <rw@neopoly.de>
2016-04-27 16:46:05 +0200

    Fix test warning

--- Assets/NguiSpriteTool/Tests/Editor/Neo/Unity/Analysis/Models/TestUISpriteInfo.cs
+++ Assets/NguiSpriteTool/Tests/Editor/Neo/Unity/Analysis/Models/TestUISpriteInfo.cs
@@ -1,13 +1,14 @@
 ﻿using NUnit.Framework;
 using Neo.Unity.Analysis.Models;
 using Tests.Neo.Unity.Analysis.Models.Base;
+using UnityEngine;
 
 namespace Tests.Neo.Unity.Analysis.Models {
   public class TestUISpriteInfo : BaseComponentInfoTest<UISprite> {
 
     [SetUp]
     public override void SetUp() {
-      component = new UISprite();
+      component = new GameObject().AddComponent<UISprite>();
       location = "UISpriteLocation";
       info = new UISpriteInfo(component, location);
     }
