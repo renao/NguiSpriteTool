3db009ef45cd7e9acd874300c447dc125ba0ac7c
René Wernicke <rw@neopoly.de>
2016-04-27 16:59:47 +0200

    Add UIAtlasInfo

---
+++ Assets/NguiSpriteTool/Scripts/Neo/Unity/Editor/Analysis/Models/UIAtlasInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Neo.Unity.Analysis.Models;
+
+namespace Neo.Unity.Analysis.Models {
+  public class UIAtlasInfo {
+    public UIAtlas Atlas;
+    public Dictionary<string, List<UISpriteInfo>> SpriteInfos;
+
+    public UIAtlasInfo(UIAtlas atlas) {
+      Atlas = atlas;
+      SpriteInfos = new Dictionary<string, List<UISpriteInfo>>();
+    }
+
+    public void AddSpriteInfo(UISpriteInfo spriteInfo) {
+      string spriteName = spriteInfo.Sprite.spriteName;
+      if (Atlas.name != spriteInfo.Sprite.atlas.name) return;
+
+      if (!SpriteInfos.ContainsKey(spriteName)) {
+        SpriteInfos[spriteName] = new List<UISpriteInfo>();
+      }
+      SpriteInfos[spriteName].Add(spriteInfo);
+    }
+  }
+}
---
+++ Assets/NguiSpriteTool/Tests/Editor/Neo/Unity/Analysis/Models/TestUIAtlasInfo.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using Neo.Unity.Analysis.Models;
+using UnityEngine;
+
+namespace Tests.Neo.Unity.Analysis.Models {
+  public class TestUIAtlasInfo {
+
+    private UIAtlasInfo info;
+    private UISpriteInfo spriteInfo;
+
+    [SetUp]
+    public void SetUp() {
+      spriteInfo = createSpriteInfo();
+      info = new UIAtlasInfo(spriteInfo.Sprite.atlas);
+    }
+
+    [Test]
+    public void InitsCorrectlyForAtlas() {
+      Assert.AreEqual(spriteInfo.Atlas, info.Atlas);
+      Assert.AreEqual(0, info.SpriteInfos.Count);
+    }
+
+    [Test]
+    public void AddsSpriteInfo() {
+      info.AddSpriteInfo(spriteInfo);
+
+      Assert.AreEqual(1, info.SpriteInfos.Count);
+      Assert.AreEqual(spriteInfo, info.SpriteInfos[spriteInfo.Sprite.spriteName][0]);
+    }
+
+    private UISpriteInfo createSpriteInfo() {
+      GameObject go = Resources.Load<GameObject>("ContainsSprite");
+      return new UISpriteInfo(go.GetComponent<UISprite>(), "some_location");
+    }
+  }
+}
