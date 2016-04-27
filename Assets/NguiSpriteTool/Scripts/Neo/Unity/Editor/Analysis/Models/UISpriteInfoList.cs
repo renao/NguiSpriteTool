4a99e9f8f9301761b792de9656f1877ee9a636d5
René Wernicke <rw@neopoly.de>
2016-04-27 16:41:11 +0200

    Take care of sprite atlasses inside UISpriteInfoList

--- Assets/NguiSpriteTool/Scripts/Neo/Unity/Editor/Analysis/Models/UISpriteInfoList.cs
+++ Assets/NguiSpriteTool/Scripts/Neo/Unity/Editor/Analysis/Models/UISpriteInfoList.cs
@@ -1,5 +1,29 @@
-﻿using Neo.Unity.Analysis.Models.Base;
+﻿using System.Collections.Generic;
+using Neo.Unity.Analysis.Models.Base;
+using Neo.Unity.Editor.Analysis.Models;
 
 namespace Neo.Unity.Analysis.Models {
-  public class UISpriteInfoList : ComponentInfoList<UISprite> {}
+  public class UISpriteInfoList : ComponentInfoList<UISprite> {
+
+    public Dictionary<string, UIAtlasInfo> Atlasses;
+
+
+    public UISpriteInfoList() : base() {
+      Atlasses = new Dictionary<string, UIAtlasInfo>();
+    }
+
+
+    public void Add(UISpriteInfo spriteInfo) {
+      addAtlas(spriteInfo);
+      base.Add(spriteInfo);
+    }
+
+    private void addAtlas(UISpriteInfo spriteInfo) {
+      if(!Atlasses.ContainsKey(spriteInfo.Component.atlas.name)) {
+        Atlasses[spriteInfo.Component.atlas.name] = new UIAtlasInfo(spriteInfo.Component.atlas);
+      }
+      Atlasses[spriteInfo.Component.atlas.name].AddSpriteInfo(spriteInfo);
+    }
+
+  }
 }
