3a11f8f04832549a93aca2325379ed82f381eb3c
René Wernicke <rw@neopoly.de>
2016-04-27 12:49:28 +0200

    Add generic ComponentTool

---
+++ Assets/NguiSpriteTool/Scripts/Neo/Unity/Editor/Analysis/ComponentTool.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+using Neo.IO;
+using Neo.Unity.Analysis.Models;
+using Neo.Unity.Analysis.Formatters;
+
+namespace Neo.Unity.Analysis {
+  public class ComponentTool<T> where T : Component {
+
+    public ComponentInfoList<T> Info {get; private set; }
+    internal string dataPath;
+    internal ComponentInfoFormatter formatter;
+
+
+    public ComponentTool(string dataPath=null) {
+      Info = new ComponentInfoList<T>();
+      this.dataPath = dataPath ?? Application.dataPath;
+      formatter = new ComponentInfoFormatter();
+    }
+
+    public void FetchComponentInfo(Action Callback=null) {
+      fetchComponents();
+      if(Callback != null) Callback();
+    }
+
+    protected virtual ComponentInfo<T> createComponentInfo(T component, string path) {
+      return new ComponentInfo<T>(component, path);
+    }
+
+    private void fetchComponents() {
+      fetchComponentsFromPrefabs();
+      fetchComponentsFromScene();
+    }
+
+    private void fetchComponentsFromPrefabs() {
+      List<string> prefabLocations = FileCrawler.FetchFilesRecursively(dataPath, "prefab");
+      foreach(string location in prefabLocations) {
+        string loc = relativePath(location);
+        GameObject go = UnityEditor.AssetDatabase.LoadAssetAtPath<GameObject>(loc);
+        if(go != null) {
+          fetchComponentsFromGameObject(go, loc);
+        }
+      }
+    }
+
+    private void fetchComponentsFromScene() {
+      List<string> sceneLocations = FileCrawler.FetchFilesRecursively(dataPath, "unity");
+      foreach(string location in sceneLocations) {
+        Scene currentScene = EditorSceneManager.OpenScene(relativePath(location), OpenSceneMode.Additive);
+        fetchComponentsFromScene(currentScene);
+        EditorSceneManager.CloseScene(currentScene, true);
+      }
+    }
+
+    private void fetchComponentsFromGameObject(GameObject go, string location) {
+      T[] components = go.GetComponentsInChildren<T>(true);
+      if(components != null) {
+        foreach(T component in components) {
+          Info.Add(createComponentInfo(component, location));
+        }
+      }
+    }
+
+    private void fetchComponentsFromScene(Scene scene) {
+      scene.GetRootGameObjects().ForEach((rootObject) => {
+        fetchComponentsWithScenePath(formatter.ScenePath(scene), rootObject);
+      });
+    }
+
+    private void fetchComponentsWithScenePath(string currentPath, GameObject sceneObject) {
+      foreach(T component in sceneObject.GetComponents<T>()) {
+        Info.Add(createComponentInfo(component, currentPath));
+      }
+      for(int i = 0; i < sceneObject.transform.childCount; i++) {
+        fetchComponentsWithScenePath(
+          formatter.AddChildToPath(currentPath, sceneObject),
+          sceneObject.transform.GetChild(i).gameObject
+        );
+      }
+    }
+
+    private string relativePath(string absolutePath) {
+      return absolutePath.Replace(@"\", "/").Replace(Application.dataPath, "Assets");
+    }
+  }
+}
---
+++ Assets/NguiSpriteTool/Tests/Editor/Neo/Unity/Analysis/TestComponentTool.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+using Neo.IO;
+using Neo.Unity.Analysis.Models;
+using Neo.Unity.Analysis.Formatters;
+using Neo.Unity.Analysis;
+using NUnit.Framework;
+
+namespace Tests.Neo.Unity.Analysis {
+  public class TestComponentTool {
+
+    protected ComponentTool<UISprite> tool;
+
+    [SetUp]
+    public virtual void SetUp() {
+      tool = new ComponentTool<UISprite>(Application.dataPath + @"\NguiSpriteTool\Tests\Editor\Prefabs");
+    }
+
+    [Test]
+    public void InitsCorrectly() {
+      Assert.NotNull(tool);
+      Assert.NotNull(tool.Info);
+    }
+
+    [Test]
+    public void GetsSpriteAndAtlasInfo() {
+      tool.FetchComponentInfo();
+
+      // tool.GetSpriteUsages();
+
+      // Assert.AreEqual(1, tool.Info.Atlasses.Count);
+
+
+      /*
+      TODO: Make this work.
+        
+      Assert.AreEqual(1, tool.Atlas.Count);
+      Assert.AreEqual(1, tool.Sprites.Count);
+
+      Assert.AreEqual("TestAtlas", tool.Atlas[0].name);
+      Assert.AreEqual("blue", tool.Sprites[tool.Atlas[0]][0].spriteName);
+
+      Assert.AreEqual(1, tool.Prefabs[tool.Sprites[tool.Atlas[0]][0]].Count);
+      Assert.AreEqual(@"Assets\NguiSpriteTool\Tests\Editor\Prefabs\AnotherSpriteObjectWithBlueSprite.prefab", tool.Prefabs[tool.Sprites[tool.Atlas[0]][0]][0]);
+      Assert.Fail("Add missing two sprite assertions and extend with structure for atlas, sprite and prefabs");
+      */
+    }
+
+    [Test]
+    public void ExecutesCallback() {
+      tool.FetchComponentInfo(Assert.Pass);
+      Assert.Fail();
+    }
+
+    internal class TestComponent : Component { }
+  }
+}
